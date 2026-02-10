a4bc9b088a9553a80264c54362afbf756d2b07be
allivka <allivka1920@gmail.com>
2026-02-10 23:55:55 +0300

    added PATH env parsing

--- main.go
+++ main.go
@@ -10,11 +10,34 @@ func help() {
 	os.Exit(0)
 }
 
+func parsePath(path string) (paths []string) {
+	
+	var prev int
+	
+	for i, r := range []rune(path) {
+		if r == ':' {
+			paths = append(paths, path[prev:i])
+			prev = i + 1
+			
+		}
+	}
+	
+	if prev < len(path) - 1 {
+		paths = append(paths, path[prev:len(path) - 1])
+	}
+	
+	return paths
+}
+
 func main() {
 	args := os.Args
 	
 	if len(args) < 2 || args[1] == "-h" || args[1] == "--help" {
 		help()
 	}
+	
+	paths := parsePath(os.Getenv("PATH"))
+	
+	fmt.Println(paths)
 	
 }
